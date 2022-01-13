aadb1eda6e6357cd540028b52eba1b64a297683d
Thiago Moura <thiagosmoura10@gmail.com>
2022-01-13 20:11:03 -0300

    upload de arquivos

---
+++ assets/javascript/script.js
@@ -0,0 +1,14 @@
+function abre(){
+    document.querySelector('.menu').classList.toggle('close');
+    document.querySelector('.line1').classList.toggle('close');
+    document.querySelector('.line2').classList.toggle('close');
+    document.querySelector('.line3').classList.toggle('close');
+    document.querySelector('body').classList.toggle('close');
+}
+
+const highlightMenu = () =>{
+    let tela = window.scrollY;
+
+
+}
+let tela = window.scrollY;
